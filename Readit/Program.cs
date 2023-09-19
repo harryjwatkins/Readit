2d52b04cbf7e00e72627a95a02fbaf172d50f779
harryjwatkins <harry.jw@outlook.com>
2023-09-19 21:04:43 +0100

    Add basic template for calling OpenLibrary API, take in user input

--- Readit/Program.cs
+++ Readit/Program.cs
@@ -1,1 +1,54 @@
-﻿Console.WriteLine("Hello world");
+﻿namespace Readit
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+    }
+
+    class Program
+    {
+
+        static public void Main(string[] args)
+        {
+            SearchForBook();
+        }
+
+        public static void SearchForBook()
+        {
+            string userSearchTerm = GetSearchTermFromUser();
+            var bookSearchResult = GetBookDataByTitle(userSearchTerm);
+            Console.WriteLine(bookSearchResult);
+        }
+
+        private static string GetSearchTermFromUser()
+        {
+            Console.Write("Please enter a book title");
+            string userSearchTerm = Console.ReadLine();
+            return userSearchTerm;
+        }
+
+        private static async Task<string> GetBookDataByTitle(string bookTitle)
+        {
+            var url = "https://openlibrary.org/search.json?q=" + bookTitle;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string searchResult = await response.Content.ReadAsStringAsync();
+                    return searchResult;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+    }
+}
